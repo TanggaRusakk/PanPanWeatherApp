3a3535031b51c8b23040d5e2b9cdfeb5f5cb6408
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-10-26 15:46:30 +0700

    Refactor MainActivity to use PanPanWeatherAppRoute

--- app/src/main/java/com/example/panpanweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/panpanweatherapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.panpanweatherapp.ui.route.PanPanWeatherAppRoute
 import com.example.panpanweatherapp.ui.theme.PanPanWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PanPanWeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PanPanWeatherAppRoute()
             }
         }
     }
