8911121e42208ceb45d34734c20561331847b76f
Angga Dhammika Jaya <anggadj94@gmail.com>
2025-10-26 15:45:48 +0700

    Add new app route

---
+++ app/src/main/java/com/example/panpanweatherapp/ui/route/PanPanWeatherAppRoute.kt
@@ -0,0 +1,40 @@
+package com.example.panpanweatherapp.ui.route
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.panpanweatherapp.ui.view.PanPanWeatherAppView
+
+enum class PanPanWeatherAppView() {
+    PanPanWeatherApp
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun PanPanWeatherAppRoute() {
+    val navController = rememberNavController()
+
+    Scaffold() { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = PanPanWeatherAppView.PanPanWeatherApp.name
+        ) {
+            composable(route = PanPanWeatherAppView.PanPanWeatherApp.name) {
+                PanPanWeatherAppView(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentPadding = innerPadding
+                )
+            }
+        }
+
+    }
+}
